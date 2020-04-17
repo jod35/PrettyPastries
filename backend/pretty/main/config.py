55feb62aace9e542fe4b965330fa4f77e86b2eb2
jona <jona@jona.com>
2020-04-18 02:11:32 +0300

    simple config setup

---
+++ backend/pretty/main/config.py
@@ -0,0 +1,6 @@
+class DevConfig:
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://jona:nathanoj35@localhost/pretty'
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    SECRET_KEY='0e38eafa051718f653a20cbc'
+    SQLALCHEMY_ECHO=True
+    
