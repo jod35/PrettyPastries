56a2884f6f89dfeb9b98a7964f59ae857403133b
jona <jona@jona.com>
2020-04-18 02:48:27 +0300

    successfuly created the end points

--- backend/pretty/main/routes.py
+++ backend/pretty/main/routes.py
@@ -6,3 +6,13 @@ from flask import render_template,redirect,flash,url_for
 @app.route('/')
 def home_page():
    return render_template('index.html')
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+@app.route('/signup')
+def register():
+    return render_template('register.html')
+    
+
