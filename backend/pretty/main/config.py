b1ad98010fe8932cd7452364c68fde68594da198
jona <jona@jona.com>
2020-04-19 02:40:58 +0300

    successfully finshed the logic for the backend

--- backend/pretty/main/config.py
+++ backend/pretty/main/config.py
@@ -1,5 +1,5 @@
 class DevConfig:
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://jona:nathanoj35@localhost/pretty'
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://jona:nathanoj35@localhost/pretty_pastry'
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     SECRET_KEY='0e38eafa051718f653a20cbc'
     SQLALCHEMY_ECHO=True
