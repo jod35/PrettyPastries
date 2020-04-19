e520c5a44d374d2519d9a2f4ef781f4048e192a8
jona <jona@jona.com>
2020-04-19 03:31:25 +0300

    started the login backend

--- backend/pretty/main/auth.py
+++ backend/pretty/main/auth.py
@@ -1,6 +1,6 @@
 from .models import User
 from werkzeug.security import generate_password_hash,check_password_hash
-from flask import flash
+from flask import flash,redirect,url_for
 from . import db
 
 def create_user_account(username,email,password):
@@ -9,3 +9,19 @@ def create_user_account(username,email,password):
 
     db.session.add(new_user)
     db.session.commit()
+
+def check_user(prompt):
+    user=(User.query.filter_by(username=prompt).first() or 
+        User.query.filter_by(email=prompt).first())
+    
+    if not user:
+        flash("User doesnot exist")
+        return redirect('login')
+
+    return user
+
+    
+
+
+
+    
--- backend/pretty/main/routes.py
+++ backend/pretty/main/routes.py
@@ -2,15 +2,28 @@ from . import app,db
 from flask import render_template,redirect,flash,url_for,request
 from .models import User
 from werkzeug.security import generate_password_hash,check_password_hash
-from .auth import create_user_account
+from .auth import create_user_account,check_user
 
 #home page
 @app.route('/')
 def home_page():
    return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login',methods=['POST'])
 def login_page():
+    prompt=request.form.get('prompt')
+
+    password=request.form.get('password')
+    
+    check_user(prompt)
+    
+    user=(User.query.filter_by(username=prompt).first() or 
+        User.query.filter_by(email=prompt).first())
+    
+    if user and check_password_hash(user.password,password):
+         
+
+    
     return render_template('login.html')
 
 @app.route('/signup',methods=['GET', 'POST'])
