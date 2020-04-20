4b50d0b32278987f627f86fb87f88eb389d6ba78
jona <jona@jona.com>
2020-04-20 06:37:29 +0300

    fixed bug on login

--- backend/pretty/main/routes.py
+++ backend/pretty/main/routes.py
@@ -2,6 +2,7 @@ from . import app,db
 from flask import render_template,redirect,flash,url_for,request
 from .models import User
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import login_user,logout_user,current_user
 from .auth import create_user_account,check_user
 
 #home page
@@ -9,21 +10,16 @@ from .auth import create_user_account,check_user
 def home_page():
    return render_template('index.html')
 
-@app.route('/login',methods=['POST'])
+@app.route('/login',methods=['POST','GET'])
 def login_page():
     prompt=request.form.get('prompt')
-
     password=request.form.get('password')
-    
-    check_user(prompt)
-    
     user=(User.query.filter_by(username=prompt).first() or 
         User.query.filter_by(email=prompt).first())
     
     if user and check_password_hash(user.password,password):
-         
-
-    
+        login_user(user)
+        return "Logged In"
     return render_template('login.html')
 
 @app.route('/signup',methods=['GET', 'POST'])
@@ -45,7 +41,8 @@ def register():
             flash("Account Created Successfully!!")
             return redirect(url_for('login_page'))
     return render_template('register.html')
-    
+
+r
 
 @app.errorhandler(404)
 def not_found(error):
